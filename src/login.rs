10883270c0ab04f6316a217d25383467c18f8038
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-07-31 12:39:06 -0400

    Add 2-factor login support

--- src/login.rs
+++ src/login.rs
@@ -1,6 +1,7 @@
+use std::str::FromStr;
+
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 use tokio::time::Duration;
 
 const LOGIN_URL: &'static str = "https://www.toontownrewritten.com/api/login?format=json";
@@ -53,31 +54,7 @@ pub async fn login(
     match client.execute(initial_request).await {
         Ok(res) => {
             let response_result = res.json::<LoginResponse>().await;
-            if let Err(err) = response_result {
-                eprintln!("Login error\n{}", err);
-                return None;
-            }
-            let response = response_result.unwrap();
-
-            match response.success {
-                LoginResult::Success => Some(LoginToken {
-                    server: response.gameserver.unwrap(),
-                    cookie: response.cookie.unwrap(),
-                }),
-                LoginResult::Partial => two_factor(&client, response.response_token.unwrap()).await,
-                LoginResult::Failure => None,
-                LoginResult::Delayed => {
-                    let eta =
-                        u32::from_str(response.eta.as_ref().unwrap()).expect("parsing eta number");
-                    let position = u32::from_str(response.position.as_ref().unwrap())
-                        .expect("parsing position number");
-                    let res = queue(&client, response.queue_token.unwrap(), eta, position).await;
-                    if res.is_none() {
-                        eprintln!("An error occurred while moving through the queue.");
-                    }
-                    res
-                }
-            }
+            handle_login_response(&client, response_result).await
         }
         Err(err) => {
             eprintln!("An error occurred while executing the request.\n{}", err);
@@ -86,10 +63,83 @@ pub async fn login(
     }
 }
 
-// TODO implement two factor authentication
-#[allow(unused)]
+macro_rules! response_result_none_return {
+    ($response_result:expr) => {{
+        if let Err(err) = $response_result {
+            eprintln!("Login error\n{}", err);
+            return None;
+        }
+        $response_result.unwrap()
+    }};
+}
+
+async fn handle_login_response(
+    client: &Client,
+    response_result: reqwest::Result<LoginResponse>,
+) -> Option<LoginToken> {
+    let response = response_result_none_return!(response_result);
+    dispatch_2fa_possible(client, response).await
+}
+
+async fn dispatch_no_2fa(client: &Client, response: LoginResponse) -> Option<LoginToken> {
+    match response.success {
+        LoginResult::Success => Some(LoginToken {
+            server: response.gameserver.unwrap(),
+            cookie: response.cookie.unwrap(),
+        }),
+        LoginResult::Partial => panic!("2FA asserted not possible, but came back partial!"),
+        LoginResult::Failure => None,
+        LoginResult::Delayed => {
+            let eta = u32::from_str(response.eta.as_ref().unwrap()).expect("parsing eta number");
+            let position = u32::from_str(response.position.as_ref().unwrap())
+                .expect("parsing position number");
+            let res = queue(&client, response.queue_token.unwrap(), eta, position).await;
+            if res.is_none() {
+                eprintln!("An error occurred while moving through the queue.");
+                return None;
+            }
+            res
+        }
+    }
+}
+
+async fn dispatch_2fa_possible(client: &Client, response: LoginResponse) -> Option<LoginToken> {
+    if response.success.is_partial() {
+        two_factor(&client, response.response_token.unwrap()).await
+    } else {
+        dispatch_no_2fa(client, response).await
+    }
+}
+
 async fn two_factor(client: &Client, token: String) -> Option<LoginToken> {
-    unimplemented!()
+    let mut token = token;
+    loop {
+        let totp = rprompt::prompt_reply_stdout("2 factor TOTP code: ").expect("Reading 2fa stdin");
+        let totp_request = client
+            .post(LOGIN_URL)
+            .form(&TOTPRequest { totp, token })
+            .build()
+            .expect("Forming request 2fa");
+        match client.execute(totp_request).await {
+            Ok(res) => {
+                let response_result = res.json::<LoginResponse>().await;
+                let response = response_result_none_return!(response_result);
+                if response.success.is_partial() {
+                    println!("Unrecognized TOTP code.");
+                    token = response.response_token.unwrap();
+                } else {
+                    return dispatch_no_2fa(client, response).await;
+                }
+            }
+            Err(err) => {
+                eprintln!(
+                    "An error occurred while executing the 2fa request.\n{}",
+                    err
+                );
+                return None;
+            }
+        }
+    }
 }
 
 async fn queue(client: &Client, token: String, eta: u32, position: u32) -> Option<LoginToken> {
@@ -105,7 +155,7 @@ async fn queue(client: &Client, token: String, eta: u32, position: u32) -> Optio
                 queue_token: token.clone(),
             })
             .build()
-            .expect("Forming request");
+            .expect("Forming request queue");
 
         let resp = client.execute(queue_request).await;
         let resp = if resp.is_ok() {
@@ -151,6 +201,14 @@ struct Credentials {
 struct QueueToken {
     #[serde(rename = "queueToken")]
     queue_token: String,
+}
+
+#[derive(Serialize)]
+struct TOTPRequest {
+    #[serde(rename = "appToken")]
+    totp: String,
+    #[serde(rename = "authToken")]
+    token: String,
 }
 
 #[derive(Deserialize)]
